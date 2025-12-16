09937451faf29eccd16dfd0b3b3b6695f027c99a
Aghaz-Alam <aghazalamjmi@gmail.com>
2025-12-16 15:11:07 +0530

    Initial commit

--- 6. MemeoryManagement.cpp/MixedTopics/Shared_ptr.cpp
+++ 6. MemeoryManagement.cpp/MixedTopics/Shared_ptr.cpp
@@ -35,21 +35,54 @@ class SharedPtr {
     T& operator*() { return *ptr; }
     T* operator->() { return ptr; }
     size_t use_count() const { return *refCount; }
+
+};
+
+// Test class for operator->
+class Test {
+public:
+    int x;
+    Test(int v) : x(v) {}
+
+    void show() {
+        cout << "Value of x in Test Class= " << x << endl;
+    }
 };
 int main() {
     SharedPtr<int> p1(new int(10));
-    cout << p1.use_count() << endl; // 1
-    {
-        SharedPtr<int> p2 = p1;
-        cout << p1.use_count() << endl; // 2
-    }
-    cout << p1.use_count() << endl; // back to 1
+    
+    // operator*
+    cout<<"p1 before modification: "<<*p1<<endl;
+    *p1 = 20;   // modifies the underlying object
+    cout<<"p1 after modification: "<<*p1<<endl;
+    
+    // Copy
+    cout << "p1.use_count(): "<<p1.use_count() << endl; // 1
+    SharedPtr<int> p2 = p1;        
+    cout << "p2.use_count(): "<<p2.use_count() << endl; // 2
+    
+
+    // Manual release of p2
+    p2.release();
+    cout << "After p2.release(), \np1.use_count(): " << p1.use_count() << endl;
+    
+    
+    // operator->
+    SharedPtr<Test> obj(new Test(100));
+    obj->show();      // calls operator->()
+
+
+
   return 0;
 }
 /* 
-1
-2
-1
+p1 before modification: 10
+p1 after modification: 20
+p1.use_count(): 1
+p2.use_count(): 2
+After p2.release(), 
+p1.use_count(): 1
+Value of x in Test Class= 100
 */
 
 
